532eb9760f84be66519ad6c6e6bfec69bc4b9fe6
verdantpath <m_dittmann@yahoo.com>
2018-06-03 21:31:44 -0700

    update my-scripts.js

--- scripts/my-scripts.js
+++ scripts/my-scripts.js
@@ -12,6 +12,11 @@ $(document).ready(function() {
   // Named function that hides the discount code
   var hideCode = function() {
     var numRand = getRandom(4);
+
+    // .each handler that loops through each .guess_box
+    // index refers to which item we are in the loop
+    // value refers to the current object (.guess_box)
+    // if the current .guess_box index mathces the random number in numRand, the discount gets appended with .append()
     $('.guess_box').each(function(index, value) {
       if(numRand == index) {
         $(this).append('<span id="has_discount"></span>');
@@ -27,7 +32,7 @@ $(document).ready(function() {
   // Another named function that tells you if you clicked the right .guess_box and what the discount is
   function checkForCode() {
     var discount;
-    
+
     // Conditional logic to see if the user found the discount code
     // *this* refers to the current element, the one that called the function, in other words the .guess_box that was clicked
     // the *contains8 method looks to see if the .guess_box that was clicked has a child with an id of "has_discount"
